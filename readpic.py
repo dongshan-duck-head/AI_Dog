6e1da4c9263836941f46c97c8265843ce2d5af24
kidneyweakx <kidneyweakx@gmail.com>
2019-03-05 11:33:38 +0800

    first commit

---
+++ readpic.py
@@ -0,0 +1,27 @@
+
+import os
+import pandas as pd
+import cv2
+from glob import glob
+
+#檔案位置
+DATAS_FOLDER = "./in"
+BLOCK_DATA_FOLDER = os.path.join(DATAS_FOLDER, "dog")
+
+#讀取檔案
+images_per_class = {}
+for class_folder_name in os.listdir(BLOCK_DATA_FOLDER):
+    class_folder_path = os.path.join(BLOCK_DATA_FOLDER, class_folder_name)
+    class_label = class_folder_name
+    images_per_class[class_label] = []
+    for image_path in glob(os.path.join(class_folder_path, "*.png")):
+        image_bgr = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        images_per_class[class_label].append(image_bgr)
+
+for key,value in images_per_class.items():
+    print("{0} -> {1}".format(key, len(value))) 
+
+
+#train
+
+#
